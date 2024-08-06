bf2f0ecdc29acb053e9a3b0ca1e098329e5d6efd
Rajia Rani <rajiarani.syal@gmail.com>
2024-08-05 21:38:52 -0500

    Remove my-blog directory from repository

--- my-blog/src/pages/AboutPage.js
+++
@@ -1,6 +0,0 @@
-const AboutPage = () => {
-    return (
-        <h1>This  is a about page</h1>
-    )
-}
-export default AboutPage;
--- my-blog/src/pages/NotFoundPage.js
+++
@@ -1,8 +0,0 @@
-const NotFoundPage = () => {
-    return (
-        <>
-        <h1>404, Opps!! Page Not Found.</h1>
-        </>
-    )
-};
-export default NotFoundPage;
